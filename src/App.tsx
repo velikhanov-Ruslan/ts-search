e539bcace571054fc1a3610befcb6aa5854b7ec5
unknown <authory313@gmail.com>
2022-08-09 16:20:16 +0400

    a

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     <Routes>
       <Route path={`/`} element={<HomePage/>}/>
       <Route path={`favorites`} element={<FavoritesPage/>}/>
-      <Route path={`*`} element={<p>Страница не найдена</p>}/>
+      <Route path={`*`} element={<HomePage/>}/>
     </Routes>
       </>
   );
